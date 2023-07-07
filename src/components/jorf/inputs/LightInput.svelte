56df258be5d906aabf5413f2b9eb186b54f2dbf2
John Kestner <john@coloured.net>
2023-07-07 11:36:00 -0500

    add range slider

--- src/components/jorf/inputs/LightInput.svelte
+++ src/components/jorf/inputs/LightInput.svelte
@@ -1,8 +1,12 @@
 <script>
   import Literal from "@components/jorf/Literal.svelte";
   import Input from "./Input.svelte";
+  import RangeSlider from "svelte-range-slider-pips";
 
-  export let value;
+  export let value = 1000;
+
+  $: values = [value];
+  let range = [0, 4000];
 
   export function summary() {
     return "light " + value;
@@ -10,10 +14,20 @@
   function sample() {
     //TODO: make clear that sampling will just get the lefthand sensor's current value. disable if it's not a dynamic value.
     let v = Math.floor(Math.random() * 100) * 10;
-    value = { value: v, label: v };
+    // value = { value: v, label: v };
   }
 </script>
 
 <Input {sample}>
-  <Literal bind:value autocomplete />
+  <div style="width:250px; height: 50px;">
+    <!-- <Literal bind:value autocomplete /> -->
+    <RangeSlider
+      bind:values
+      style="width:200px; height: 50px;"
+      min={range[0]}
+      max={range[1]}
+      all="label"
+    />
+    {console.log(values, value)}
+  </div>
 </Input>
