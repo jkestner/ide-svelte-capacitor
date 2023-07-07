5099b50c3ffbf0d14f44699f88249c107a681f0b
John Kestner <john@coloured.net>
2023-07-07 11:36:16 -0500

    warm amber theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,17 @@ module.exports = {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [      {
+      pickup: {
+        "primary": "#FF9E00",
+        "secondary": "#00CBFF",
+        "accent": "#FF3800",
+        "neutral": "#222222",
+        "base-100": "#FFF9F0", //"#FFFFFF"
+      },
+    },
+"bumblebee", "retro"],
+  },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
 }
