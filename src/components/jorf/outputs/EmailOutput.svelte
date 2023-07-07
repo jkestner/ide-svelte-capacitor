7be00927f9f6acee88d166cb2646395c7bd75529
John Kestner <john@coloured.net>
2023-07-07 11:40:52 -0500

    add test to email output, debug screen size

--- src/components/jorf/outputs/EmailOutput.svelte
+++ src/components/jorf/outputs/EmailOutput.svelte
@@ -1,6 +1,7 @@
 <script>
   import Output from "./Output.svelte";
   import VariableInserter from "@components/VariableInserter.svelte";
+  import FaCompressArrowsAlt from "svelte-icons/fa/FaCompressArrowsAlt.svelte";
 
   export let params = { to: "", content: "" };
 
@@ -28,9 +29,14 @@
       type="text"
       rows="1"
       placeholder="content"
-      class="textarea textarea-bordered"
+      class="textarea textarea-bordered rounded-none"
       bind:value={params.content}
     />
+    {#if test}
+      <button class="btn w-14" alt="insert?" on:click={test}
+        ><FaCompressArrowsAlt /></button
+      >
+    {/if}
   </label>
   <!-- <div class="indicator-item pt-6">
       <VariableInserter />
